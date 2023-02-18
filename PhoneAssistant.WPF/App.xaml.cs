d982be6872c97be71ce7efadd165a79a57403bef
JLdgu <Jonathan.Linstead@devon.gov.uk>
2023-02-18 09:12:13 +0000

    Add Extensions packages

--- PhoneAssistant.WPF/App.xaml.cs
+++ PhoneAssistant.WPF/App.xaml.cs
@@ -1,4 +1,12 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PhoneAssistant.WPF.ViewModels;
+using System.Configuration;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Navigation;
+using System.Windows.Threading;
 
 namespace PhoneAssistant.WPF;
 /// <summary>
@@ -6,4 +14,91 @@ namespace PhoneAssistant.WPF;
 /// </summary>
 public partial class App : Application
 {
+    private IHost _host;
+
+    public App()
+    {
+        
+    }
+
+    private async void OnStartup(object sender, StartupEventArgs e)
+    {
+        //var appLocation = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
+        // For more information about .NET generic host see  https://docs.microsoft.com/aspnet/core/fundamentals/host/generic-host?view=aspnetcore-3.0
+        
+        _host = Host.CreateDefaultBuilder(e.Args)
+                .ConfigureAppConfiguration(c =>
+                {
+                    //c.SetBasePath(appLocation);
+                })
+                .ConfigureServices(ConfigureServices)
+                .Build();
+
+        
+        await _host.StartAsync();
+
+        MainWindow = _host.Services.GetRequiredService<MainWindow>();
+        MainWindow.Show();
+    }
+
+    private void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+    {
+        string? connectionString = context.Configuration.GetConnectionString("Default");
+
+        // App Host
+        //services.AddHostedService<ApplicationHostService>();
+
+        // Activation Handlers
+
+        // Core Services
+        //services.AddSingleton<IFileService, FileService>();
+
+        // Services
+        //services.AddSingleton<IApplicationInfoService, ApplicationInfoService>();
+        //services.AddSingleton<ISystemService, SystemService>();
+        //services.AddSingleton<IPersistAndRestoreService, PersistAndRestoreService>();
+        //services.AddSingleton<IThemeSelectorService, ThemeSelectorService>();
+        //services.AddSingleton<ISampleDataService, SampleDataService>();
+        //services.AddSingleton<IPageService, PageService>();
+        //services.AddSingleton<INavigationService, NavigationService>();
+
+        // Windows
+        services.AddSingleton<MainWindow>();
+
+        // Views and ViewModels
+        //services.AddTransient<IShellWindow, ShellWindow>();
+        //services.AddTransient<ShellViewModel>();
+
+        //services.AddTransient<MainViewModel>();
+        //services.AddTransient<MainPage>();
+
+        //services.AddTransient<BlankViewModel>();
+        //services.AddTransient<BlankPage>();
+
+        //services.AddTransient<ListDetailsViewModel>();
+        //services.AddTransient<ListDetailsPage>();
+
+        //services.AddTransient<DataGridViewModel>();
+        //services.AddTransient<DataGridPage>();
+
+        //services.AddTransient<SettingsViewModel>();
+        //services.AddTransient<SettingsPage>();
+
+        //// Configuration
+        //services.Configure<AppConfig>(context.Configuration.GetSection(nameof(AppConfig)));
+    }
+
+    private async void OnExit(object sender, ExitEventArgs e)
+    {
+        await _host.StopAsync();
+        _host.Dispose();
+        _host = null;
+    }
+
+    private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        // TODO: Please log and handle the exception as appropriate to your scenario
+        // For more info see https://docs.microsoft.com/dotnet/api/system.windows.application.dispatcherunhandledexception?view=netcore-3.0
+    }
 }

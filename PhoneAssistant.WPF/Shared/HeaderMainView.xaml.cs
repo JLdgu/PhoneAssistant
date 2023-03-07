f898121ae151775b7d781a1c863684e8e2e96222
JLdgu <Jonathan.Linstead@devon.gov.uk>
2023-03-07 09:38:15 +0000

    Rename MainViewHeader

---
+++ PhoneAssistant.WPF/Shared/HeaderMainView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PhoneAssistant.WPF.Shared
+{
+    /// <summary>
+    /// Interaction logic for HeaderMainView.xaml
+    /// </summary>
+    public partial class HeaderMainView : UserControl
+    {
+        public static readonly DependencyProperty HeaderTextProperty = DependencyProperty.Register(
+            "HeaderText", typeof(string), typeof(HeaderMainView), new PropertyMetadata(default(string)));
+
+        public static readonly DependencyProperty HeaderIconProperty = DependencyProperty.Register(
+            "HeaderIcon", typeof(string), typeof(HeaderMainView), new PropertyMetadata(default(string)));
+
+        public string HeaderText
+        {
+            get { return (string)GetValue(HeaderTextProperty); }
+            set { SetValue(HeaderTextProperty, value); }
+        }
+
+        public string HeaderIcon
+        {
+            get { return (string)GetValue(HeaderIconProperty); }
+            set { SetValue(HeaderIconProperty, value); }
+        }
+
+        public HeaderMainView()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}

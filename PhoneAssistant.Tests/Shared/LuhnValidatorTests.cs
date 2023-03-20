9579e39adafdd5ab458e061694afd1f7f21bd0b5
JLdgu <Jonathan.Linstead@devon.gov.uk>
2023-03-20 17:44:41 +0000

    Add Luhn Validator and tests

---
+++ PhoneAssistant.Tests/Shared/LuhnValidatorTests.cs
@@ -0,0 +1,84 @@
+﻿using PhoneAssistant.WPF.Shared;
+
+namespace PhoneAssistant.Tests.Shared;
+
+[TestClass]
+public sealed class LuhnValidatorTests
+{
+    [TestMethod]
+    [DataRow(0)]
+    [DataRow(1)]
+    public void IsValid_ThrowsException_When_requiredLength_Invalid(int requiredLength)
+    {
+        const string input = "aaa";
+
+        var actual = Assert.ThrowsException<ArgumentException>(() => LuhnValidator.IsValid(input, requiredLength));
+
+        Assert.AreEqual("length must be omitted or > 1", actual.Message);
+    }
+
+    [TestMethod]
+    public void IsValid_ThrowsException_When_luhnString_ShorterThan_RequiredLength()
+    {
+        const string input = "361568";
+
+        var actual = Assert.ThrowsException<ArgumentException>(() => LuhnValidator.IsValid(input, 99));
+
+        Assert.AreEqual("Length of luhnString != length", actual.Message);
+    }
+
+    [TestMethod]
+    public void IsValid_ThrowsException_When_luhnString_LongerThan_RequiredLength()
+    {
+        const string input = "361568";
+
+        var actual = Assert.ThrowsException<ArgumentException>(() => LuhnValidator.IsValid(input, 3));
+
+        Assert.AreEqual("Length of luhnString != length", actual.Message);
+    }
+
+    [TestMethod]
+    public void IsValid_ReturnsFalse_WhenInputNull()
+    {
+        string? input = null;
+
+        var actual = LuhnValidator.IsValid(input);
+
+        Assert.IsFalse(actual);
+    }
+
+    [TestMethod]
+    public void IsValid_ReturnsFalse_WhenInputOneDigit()
+    {
+        string input = "7";
+
+        var actual = LuhnValidator.IsValid(input);
+
+        Assert.IsFalse(actual);
+
+    }
+    [TestMethod]
+    public void IsValid_ReturnsFalse_WhenInputNotNumeric()
+    {
+        string input = "6a";
+
+        var actual = LuhnValidator.IsValid(input);
+
+        Assert.IsFalse(actual);
+    }
+
+    [TestMethod]
+    [DataRow("79927398713")]
+    [DataRow("35145120840121")]
+    [DataRow("361568")]
+    [DataRow("361576")]
+    [DataRow("4012888888881881")]
+    public void IsValid_ReturnsTrue_WhenInputValid(string luhnString)
+    {        
+        // Act
+        var actual = LuhnValidator.IsValid(luhnString);
+
+        //Assert
+        Assert.IsTrue(actual);
+    }
+}
---
+++ PhoneAssistant.WPF/Shared/LuhnValidator.cs
@@ -0,0 +1,56 @@
+﻿namespace PhoneAssistant.WPF.Shared;
+
+public static  class LuhnValidator
+{
+    /// <summary>
+    /// This class uses the Luhn algorithm to validate a string
+    /// </summary>
+    /// <param name="luhn">IMEI/SIM card number</param>
+    /// 
+    /// <param name="length">The expected length of the luhn parameter</param>
+    /// <returns>bool</returns>
+    public static bool IsValid(string? luhn, int length = -1)
+    {
+        if (luhn is null)
+            return false;
+
+        if (luhn.Length == 1)
+            return false;
+
+        if (length == 0 || length == 1)
+            throw new ArgumentException("length must be omitted or > 1");
+        
+        if (length > 1 && luhn.Length != length)
+            throw new ArgumentException("Length of luhnString != length");
+
+        long result;
+        if (!Int64.TryParse(luhn, out result))
+            return false;
+
+        int inputCheckDigit = int.Parse(luhn.Substring(luhn.Length -1 , 1));        
+
+        int sum = 0;
+        bool parityDigit = luhn.Length % 2 == 0; 
+        for (int index = 0; index < luhn.Length - 1; index++)
+        //bool parityDigit = true;
+        //for (int index = luhnString.Length - 2; index >= 0; index--)
+        {
+            int digit = int.Parse(luhn.Substring(index, 1));
+
+            if (parityDigit)
+            {
+                digit *= 2;
+
+                if (digit > 9)
+                {
+                    digit = (digit % 10) + 1;
+                }
+            }
+            sum += digit;
+            parityDigit = !parityDigit;
+        }
+        int calculatedCheckDigit = (10 - (sum % 10)) % 10;
+
+        return (inputCheckDigit == calculatedCheckDigit);
+    }
+}

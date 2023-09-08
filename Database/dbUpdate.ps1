92c1745ab2a3b9b0bc9fe266e1dcc0a1244782d8
JLdgu <jonathan.linstead@devon.gov.uk>
2023-09-08 07:58:00 +0100

    Split to rename columns and add role

--- Database/dbUpdate.ps1
+++ Database/dbUpdate.ps1
@@ -2,11 +2,12 @@ param (
     [string]$db = "test"
 )
 
-$script = "RenameColumnsRole.sql"
+$script = "RenameColumns.sql"
 
 switch ($db.ToLower()) {
     "test" { $dbPath =  "c:/temp/paTest.db"  }
-    Default { $dbPath = "p:/ICTS/Mobile Phones/PhoneAssistant/PhoneAssistant.db" }    }
+    "live" { $dbPath = "p:/ICTS/Mobile Phones/PhoneAssistant/PhoneAssistant.db" }    
+}
 
 $env:Path += ';c:\temp\sqlite'
 Write-Host "Database $dbPath"

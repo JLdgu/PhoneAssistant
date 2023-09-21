2bb8eb93f4c9a79e860c1ed187932f6fa18ec70f
JLdgu <jonathan.linstead@devon.gov.uk>
2023-09-21 11:55:59 +0100

    Complete ThemeWrapper

--- PhoneAssistant.Tests/Features/Settings/SettingsMainViewModelTests.cs
+++ PhoneAssistant.Tests/Features/Settings/SettingsMainViewModelTests.cs
@@ -1,4 +1,5 @@
 ﻿using Moq;
+using Moq.AutoMock;
 
 using PhoneAssistant.WPF.Application;
 using PhoneAssistant.WPF.Features.Settings;
@@ -9,119 +10,90 @@ namespace PhoneAssistant.Tests.Features.Settings;
 
 public sealed class SettingsMainViewModelTests
 {
-    [Fact]
-    public void Constructor_SetsVMProperties()
+    [Theory]
+    [InlineData(true,true)]
+    [InlineData(false,false)]
+    public void Constructor_SetsVMProperties(bool printToFile, bool darkMode)
     {
         const string VERSION = "0.0.0.1";
-        Mock<IUserSettings> userSettings = new();
-        userSettings.Setup(s => s.AssemblyVersion).Returns(new Version(VERSION));
-        userSettings.Setup(s => s.PrintToFile).Returns(false);
+        AutoMocker mocker = new AutoMocker();
+        Mock<IUserSettings> userSettings = mocker.GetMock<IUserSettings>();
+        userSettings.Setup(s => s.Database).Returns("Database");
+        userSettings.Setup(s => s.PrintToFile).Returns(printToFile);
         userSettings.Setup(s => s.Printer).Returns("Printer");
         userSettings.Setup(s => s.PrintFile).Returns("PrintFile");
-        userSettings.Setup(s => s.DarkMode).Returns(true);
-        Mock<IThemeWrapper> themeWrapper = new();
-        var vm = new SettingsMainViewModel(userSettings.Object, themeWrapper.Object);
+        userSettings.Setup(s => s.DarkMode).Returns(darkMode);
+        userSettings.Setup(s => s.AssemblyVersion).Returns(new Version(VERSION));
+        SettingsMainViewModel vm = mocker.CreateInstance<SettingsMainViewModel>();
 
-        Xunit.Assert.True(vm.PrintToPrinter);
-        Xunit.Assert.False(vm.PrintToFile);
-        Xunit.Assert.Equal("Printer", vm.Printer);
-        Xunit.Assert.Equal("PrintFile", vm.PrintFile);
-
-        Xunit.Assert.False(vm.ColourThemeLight);
-        Xunit.Assert.True(vm.ColourThemeDark);
-
-        Xunit.Assert.Equal(VERSION, vm.VersionDescription);
-
+        Assert.Equal(printToFile, vm.PrintToFile);
+        Assert.Equal(!printToFile, vm.PrintToPrinter);
+        Assert.Equal("Printer", vm.Printer);
+        Assert.Equal("PrintFile", vm.PrintFile);
+        Assert.Equal(darkMode,vm.ColourThemeDark);
+        Assert.Equal(!darkMode,vm.ColourThemeLight);
+        Assert.Equal(VERSION, vm.VersionDescription);
         userSettings.VerifyGet(s => s.AssemblyVersion);
-    }
-
-    [Fact]
-    public void Constructor_SettingPrintToFileFalse_SetsPrintToPrinterTrue()
-    {
-        var mock = new Mock<IUserSettings>();
-        mock.Setup(s => s.PrintToFile).Returns(false);
-        Mock<IThemeWrapper> themeWrapper = new();
-        var vm = new SettingsMainViewModel(mock.Object, themeWrapper.Object);
-
-
-        Xunit.Assert.True(vm.PrintToPrinter);
-        Xunit.Assert.False(vm.PrintToFile);
-    }
-
-    [Fact]
-    public void Constructor_SettingPrintToFileTrue_SetsPrintToFileTrue()
-    {
-        var mock = new Mock<IUserSettings>();
-        mock.Setup(s => s.PrintToFile).Returns(true);
-
-        Mock<IThemeWrapper> themeWrapper = new();
-        var vm = new SettingsMainViewModel(mock.Object, themeWrapper.Object);
-
-
-        Xunit.Assert.True(vm.PrintToFile);
-        Xunit.Assert.False(vm.PrintToPrinter);
     }
 
     [Fact]
     public void Database_PropertyChanged_SavesUpdatedSettings()
     {
-        var mock = new Mock<IUserSettings>();
-        mock.Setup(s => s.Database).Returns("Database");
-        Mock<IThemeWrapper> themeWrapper = new();
-        var vm = new SettingsMainViewModel(mock.Object, themeWrapper.Object);
+        AutoMocker mocker = new AutoMocker();
+        Mock<IUserSettings> userStettingsMock = mocker.GetMock<IUserSettings>();
+        userStettingsMock.Setup(s => s.Database).Returns("Database");
+        SettingsMainViewModel vm = mocker.CreateInstance<SettingsMainViewModel>();
 
         const string NEW_VALUE = "Database Changed";
-
         vm.Database = NEW_VALUE;
-
-        mock.VerifySet(s => s.Database = NEW_VALUE);
-        mock.Verify(s => s.Save(), Times.Once());
+        
+        userStettingsMock.VerifySet(s => s.Database = NEW_VALUE);
+        userStettingsMock.Verify(s => s.Save(), Times.Once());
     }
 
     [Fact]
     public void Printer_PropertyChanged_SavesUpdatedSettings()
     {
-        var mock = new Mock<IUserSettings>();
-        mock.Setup(s => s.Printer).Returns("Printer");
-        Mock<IThemeWrapper> themeWrapper = new();
-        var vm = new SettingsMainViewModel(mock.Object, themeWrapper.Object);
+        AutoMocker mocker = new AutoMocker();
+        Mock<IUserSettings> userStettingsMock = mocker.GetMock<IUserSettings>();
+        userStettingsMock.Setup(s => s.Printer).Returns("Printer");
+        SettingsMainViewModel vm = mocker.CreateInstance<SettingsMainViewModel>();
 
         const string NEW_VALUE = "Printer Changed";
-
         vm.Printer = NEW_VALUE;
 
-        mock.VerifySet(s => s.Printer = NEW_VALUE);
-        mock.Verify(s => s.Save(), Times.Once());
+        userStettingsMock.VerifySet(s => s.Printer = NEW_VALUE);
+        userStettingsMock.Verify(s => s.Save(), Times.Once());
     }
 
     [Fact]
     public void PrinterFile_PropertyChanged_SavesUpdatedSettings()
     {
-        var mock = new Mock<IUserSettings>();
-        mock.Setup(s => s.PrintFile).Returns("PrintFile");
-        Mock<IThemeWrapper> themeWrapper = new();
-        var vm = new SettingsMainViewModel(mock.Object, themeWrapper.Object);
+        AutoMocker mocker = new AutoMocker();
+        Mock<IUserSettings> userStettingsMock = mocker.GetMock<IUserSettings>();
+        userStettingsMock.Setup(s => s.PrintFile).Returns("PrintFile");
+        SettingsMainViewModel vm = mocker.CreateInstance<SettingsMainViewModel>();
 
         const string NEW_VALUE = "PrintFile Changed";
-
         vm.PrintFile = NEW_VALUE;
 
-        mock.VerifySet(s => s.PrintFile = NEW_VALUE);
-        mock.Verify(s => s.Save(), Times.Once());
+        userStettingsMock.VerifySet(s => s.PrintFile = NEW_VALUE);
+        userStettingsMock.Verify(s => s.Save(), Times.Once());
     }
 
     [Fact]
     public void ColourThemeDark_PropertyChanged_SavesUpdatedSettings()
     {
-        var mock = new Mock<IUserSettings>();
-        mock.Setup(s => s.DarkMode).Returns(false);
-        Mock<IThemeWrapper> themeWrapper = new();
-        var vm = new SettingsMainViewModel(mock.Object, themeWrapper.Object);
+        AutoMocker mocker = new AutoMocker();
+        Mock<IUserSettings> userStettingsMock = mocker.GetMock<IUserSettings>();
+        userStettingsMock.Setup(s => s.DarkMode).Returns(false);
+        Mock<IThemeWrapper> themeWrapperMock = mocker.GetMock<IThemeWrapper>();
+        SettingsMainViewModel vm = mocker.CreateInstance<SettingsMainViewModel>();
 
         vm.ColourThemeDark = true;
 
-        mock.VerifySet(s => s.DarkMode = true);
-        mock.Verify(s => s.Save(), Times.Once());
-
+        userStettingsMock.VerifySet(s => s.DarkMode = true);
+        userStettingsMock.Verify(s => s.Save(), Times.Once());
+        themeWrapperMock.Verify(t => t.ModifyTheme(true),Times.Once());
     }
 }

f13e9ca3ceb8fbf7aad6b5d79c09ed1d6708e04b
JLdgu <jonathan.linstead@devon.gov.uk>
2023-12-19 14:39:16 +0000

    v1.12.19.0

--- publish.ps1
+++ publish.ps1
@@ -1,1 +1,1 @@
-dotnet publish ./PhoneAssistant.WPF/PhoneAssistant.WPF.csproj -c Release -r win10-x64 --self-contained false -o publish
+dotnet publish ./PhoneAssistant.WPF/PhoneAssistant.WPF.csproj -c Release -r win-x64 --self-contained false -o publish

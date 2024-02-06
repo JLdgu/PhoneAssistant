7d7cebbb56cb7c3c6da5c4e97dcb74ef401b5fb9
JLdgu <jonathan.linstead@devon.gov.uk>
2024-02-06 12:49:53 +0000

    Add new classes

---
+++ PhoneAssistant.WPF/Application/Repositories/DisposalsRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneAssistant.WPF.Application.Entities;
+
+namespace PhoneAssistant.WPF.Application.Repositories;
+
+public sealed class DisposalsRepository
+{
+    private readonly PhoneAssistantDbContext _dbContext;
+
+    public DisposalsRepository(PhoneAssistantDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Disposal?> GetDisposal(string imei)
+    {
+        Disposal? disposal = await _dbContext.Disposals.FirstOrDefaultAsync(d => d.Imei == imei);
+        return disposal;
+    }
+
+    public void Add(Disposal disposal)
+    {
+        _dbContext.Disposals.Add(disposal);
+        //await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<bool> Save()
+    {
+        try
+        {
+            await _dbContext.SaveChangesAsync();
+            return false;
+        }
+        catch (Exception ex) 
+        { 
+            return true; 
+        }
+    }
+
+}

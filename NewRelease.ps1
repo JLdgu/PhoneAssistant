da866a29310d87f99cea955a1570fa4ac99e834b
JLdgu <jonathan.linstead@devon.gov.uk>
2024-02-16 08:22:02 +0000

    Final changes to email view
    Closes #11

--- NewRelease.ps1
+++ NewRelease.ps1
@@ -1,12 +1,14 @@
-$newRelease = "K:\FITProject\ICTS\Mobile Phones\PhoneAssistant\Application\v1.401.24.0"
+#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
 
-If (Test-Path -Path $newRelease)
-{    
-    $newRelease = $newRelease + "\*"
-    New-Item -Path "$ENV:UserProfile\AppData\Local" -Name PhoneAssistant.New -ItemType Directory
-    Copy-Item -Path $newRelease -Destination "$ENV:UserProfile\AppData\Local\PhoneAssistant.New"
-}
-else
-{
+$newRelease = "K:\FITProject\ICTS\Mobile Phones\PhoneAssistant\Application\v1.402.16"
+
+If (!(Test-Path -Path $newRelease))
+{   
+    # Copy latest to K:\...
     Copy-Item -Path "c:\dev\PhoneAssistant\Publish" -Destination $newRelease -Recurse
 }
+
+# Copy to c:\Users\...
+$newRelease = $newRelease + "\*"
+New-Item -Path "$ENV:UserProfile\AppData\Local" -Name PhoneAssistant.New -ItemType Directory -ErrorAction SilentlyContinue
+Copy-Item -Path $newRelease -Destination "$ENV:UserProfile\AppData\Local\PhoneAssistant.New" -Recurse

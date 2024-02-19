930a2cd59297effc1d883e52681d6c1919c9ece6
JLdgu <jonathan.linstead@devon.gov.uk>
2024-02-19 12:19:31 +0000

    Change database update script to take parameters

--- Database/dbUpdate.ps1
+++ Database/dbUpdate.ps1
@@ -1,9 +1,32 @@
+param
+(
+   [Parameter(Position=0,mandatory=$true)]
+   [string] $name,
+   [Parameter(Position=1)]
+   [bool] $test = $true
+)
 
-$script = "Migrations/M20240212_BaseReport.sql"
+$scriptPath = (Get-Item .).FullName
 
-$dbPath =  "c:/temp/paTest.db"  
-#$dbPath = "p:/ICTS/Mobile Phones/PhoneAssistant/PhoneAssistant.db" 
+$script = Join-Path $scriptPath "Migrations" 
+$script = Join-Path $script "$name.sql"
+
+If (!(Test-Path -Path $script))
+{   
+    Write-Host "$script not found"
+    Exit
+}
+
+if ($test)
+{
+    $dbPath =  "c:/temp/paTest.db"  
+}
+else
+{
+    Write-Host "LIVE run" 
+    #$dbPath = "p:/ICTS/Mobile Phones/PhoneAssistant/PhoneAssistant.db" 
+}
 
 $env:Path += ';c:\temp\sqlite'
-Write-Host "Database $dbPath"
+Write-Host "Database $dbPath" 
 sqlite3 $dbPath -init $script

4784ccb839c7de85121be38e6024d90b94b6c8ca
JLdgu <jonathan.linstead@devon.gov.uk>
2024-05-13 13:13:58 +0100

    Migrate Disposal

---
+++ PhoneAssistant.WPF/Migrations/20240513121222_Disposal.sql
@@ -0,0 +1,17 @@
+﻿BEGIN TRANSACTION;
+
+CREATE TABLE "ReconcileDisposals" (
+    "Imei" TEXT NOT NULL CONSTRAINT "PK_ReconcileDisposals" PRIMARY KEY,
+    "StatusMS" TEXT NULL,
+    "StatusPA" TEXT NULL,
+    "StatusSCC" TEXT NULL,
+    "SR" INTEGER NULL,
+    "Certificate" INTEGER NULL,
+    "Action" TEXT NULL
+);
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20240513121222_Disposal', '8.0.4');
+
+COMMIT;
+

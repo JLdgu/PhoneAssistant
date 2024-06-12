b8170184d42405348cf9f77f62e973197a17a764
JLdgu <jonathan.linstead@devon.gov.uk>
2024-06-12 12:47:17 +0100

    Remove base report scripts

--- Database/BaseReport/BaseReport.sql
+++
@@ -1,8 +0,0 @@
-.open c:/temp/phoneassistant.db
-.mode csv
-.import c:/temp/DevonBaseReport.csv temp_table
-CREATE TABLE BaseReport ([Telephone Number], [User Name], [Contract End Date], [Current Talkplan Name], [Handset Manufacturer], [Sim Number], [Connected IMEI], [Last Used IMEI]); 
-INSERT INTO BaseReport ([Telephone Number], [User Name], [Contract End Date], [Current Talkplan Name], [Handset Manufacturer], [Sim Number], [Connected IMEI], [Last Used IMEI])
-SELECT [Telephone Number], [User Name], [Contract End Date], [Current Talkplan Name], [Handset Manufacturer], [Sim Number], [Connected IMEI], [Last Used IMEI]
-FROM temp_table;
-DROP TABLE temp_table;

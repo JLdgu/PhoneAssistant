02ecb296318f01d68f1b323d69fc8be3510c234a
JLdgu <jonathan.linstead@devon.gov.uk>
2024-09-10 12:17:43 +0100

    Add beta channel to Velopack

--- NewRelease.ps1
+++ NewRelease.ps1
@@ -1,7 +1,7 @@
 #Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
 
-vpk pack -u PhoneAssistant -v 0.408.23 -p .\publish -e PhoneAssistant.exe -i PhoneAssistant.WPF\Resources\Phone.ico --packAuthors "Devon County Council" --noPortable 
+vpk download local --path "\\countyhall.ds2.devon.gov.uk\docs\exeter, county hall\FITProject\ICTS\Mobile Phones\PhoneAssistant\Application"
+vpk pack -c beta -u PhoneAssistant -v 0.409.9 -p .\publish -e PhoneAssistant.exe -i PhoneAssistant.WPF\Resources\Phone.ico --packAuthors "Devon County Council" --noPortable 
 
-#Copy-Item -Path "c:\dev\PhoneAssistant\Releases\*" -Destination "\\countyhall.ds2.devon.gov.uk\docs\exeter, county hall\FITProject\ICTS\Mobile Phones\PhoneAssistant\Application" -Recurse -Force
-vpk upload local --keepMaxReleases 6 --path "\\countyhall.ds2.devon.gov.uk\docs\exeter, county hall\FITProject\ICTS\Mobile Phones\PhoneAssistant\Application"
 Return
+vpk upload local -c beta --keepMaxReleases 6 --path "\\countyhall.ds2.devon.gov.uk\docs\exeter, county hall\FITProject\ICTS\Mobile Phones\PhoneAssistant\Application"

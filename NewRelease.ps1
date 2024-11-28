3d9bcd29701caa2d70108e552bdc2fb9e66d99ba
JLdgu <jonathan.linstead@devon.gov.uk>
2024-11-28 08:49:15 +0000

    Remove channel from NewRelease script

--- NewRelease.ps1
+++ NewRelease.ps1
@@ -1,7 +1,7 @@
 #Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
 
 vpk download local --path "\\countyhall.ds2.devon.gov.uk\docs\exeter, county hall\FITProject\ICTS\Mobile Phones\PhoneAssistant\Application"
-vpk pack -c beta -u PhoneAssistant -v 0.409.9 -p .\publish -e PhoneAssistant.exe -i PhoneAssistant.WPF\Resources\Phone.ico --packAuthors "Devon County Council" --noPortable 
+vpk pack -u PhoneAssistant -v 1.411.28 -p .\publish -e PhoneAssistant.exe -i PhoneAssistant.WPF\Resources\Phone.ico --packAuthors "Devon County Council" --noPortable 
 
 Return
-vpk upload local -c beta --keepMaxReleases 6 --path "\\countyhall.ds2.devon.gov.uk\docs\exeter, county hall\FITProject\ICTS\Mobile Phones\PhoneAssistant\Application"
+vpk upload local --keepMaxReleases 6 --path "\\countyhall.ds2.devon.gov.uk\docs\exeter, county hall\FITProject\ICTS\Mobile Phones\PhoneAssistant\Application"

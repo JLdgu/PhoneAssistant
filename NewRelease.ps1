fba6ac8b2adbc51a6b1f2ec98b2234eeb08ecc5d
JLdgu <jonathan.linstead@devon.gov.uk>
2025-06-18 08:15:48 +0100

    v1.506.18

--- NewRelease.ps1
+++ NewRelease.ps1
@@ -1,5 +1,3 @@
-#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
-
 vpk download local --path "\\countyhall.ds2.devon.gov.uk\docs\exeter, county hall\FITProject\ICTS\Mobile Phones\PhoneAssistant\Application"
-vpk pack -u PhoneAssistant -v 1.506.2 -p .\publish -e PhoneAssistant.exe -i PhoneAssistant.WPF\Resources\Phone.ico --packAuthors "Devon County Council" --noPortable 
+vpk pack -u PhoneAssistant -v 1.506.18 -p .\publish -e PhoneAssistant.exe -i PhoneAssistant.WPF\Resources\Phone.ico --packAuthors "Devon County Council" --noPortable 
 vpk upload local --keepMaxReleases 6 --path "\\countyhall.ds2.devon.gov.uk\docs\exeter, county hall\FITProject\ICTS\Mobile Phones\PhoneAssistant\Application"
--- publish.ps1
+++ publish.ps1
@@ -1,2 +1,3 @@
+remove-item c:\dev\phoneassistant\publish\* -force -recurse
 dotnet publish ./PhoneAssistant.WPF/PhoneAssistant.WPF.csproj -c Release -r win-x64 --self-contained false -o publish
 dotnet publish ./Reconcile/Reconcile.csproj -c Release -r win-x64 --self-contained false -o publish

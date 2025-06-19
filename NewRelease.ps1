97bf51b3caef89e6d351f6b631ebd12949845596
JLdgu <jonathan.linstead@devon.gov.uk>
2025-06-19 11:55:15 +0100

    v1.506.19

--- NewRelease.ps1
+++ NewRelease.ps1
@@ -1,3 +1,3 @@
 vpk download local --path "\\countyhall.ds2.devon.gov.uk\docs\exeter, county hall\FITProject\ICTS\Mobile Phones\PhoneAssistant\Application"
-vpk pack -u PhoneAssistant -v 1.506.18 -p .\publish -e PhoneAssistant.exe -i PhoneAssistant.WPF\Resources\Phone.ico --packAuthors "Devon County Council" --noPortable 
+vpk pack -u PhoneAssistant -v 1.506.19 -p .\publish -e PhoneAssistant.exe -i PhoneAssistant.WPF\Resources\Phone.ico --packAuthors "Devon County Council" --noPortable 
 vpk upload local --keepMaxReleases 6 --path "\\countyhall.ds2.devon.gov.uk\docs\exeter, county hall\FITProject\ICTS\Mobile Phones\PhoneAssistant\Application"

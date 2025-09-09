fa462ce4c856f53e01647acd2fe83ffc05090391
JLdgu <jonathan.linstead@devon.gov.uk>
2025-09-09 13:17:35 +0100

    New Phone Columns

--- DbUtil/SqlScripts.cs
+++ DbUtil/SqlScripts.cs
@@ -11,7 +11,8 @@ public class SqlScripts
     {
         Scripts = [
             new Script("Drop table SchemaVersions", "DROP TABLE SchemaVersions;"),
-            new Script("Restructure Locations",RestructureLocations())
+            new Script("Restructure Locations",RestructureLocations),
+            new Script("Add 3 columns to Phones", Add3ColumnsToPhones)
         ];
     }
 
@@ -19,17 +20,32 @@ public class SqlScripts
     {
         Scripts = scripts;
     }
-
-    private string RestructureLocations()
+    private static string Add3ColumnsToPhones
     {
-        StringBuilder sb = new();
-        sb.AppendLine("ALTER TABLE Locations ADD COLUMN Note TEXT;");
-        sb.AppendLine("""
+        get
+        {
+            StringBuilder sb = new();
+            sb.AppendLine("ALTER TABLE Phones ADD COLUMN SerialNumber TEXT;");
+            sb.AppendLine("ALTER TABLE Phones ADD COLUMN eSIM INTEGER;");
+            sb.AppendLine("ALTER TABLE Phones ADD COLUMN IncludeOnTrackingSheet INTEGER;");
+
+            return sb.ToString();
+        }
+    }  
+
+    private static string RestructureLocations
+    {
+        get
+        {
+            StringBuilder sb = new();
+            sb.AppendLine("ALTER TABLE Locations ADD COLUMN Note TEXT;");
+            sb.AppendLine("""
             UPDATE Locations SET Note = '<p><br /><span style="color: red;"><b>Important Note</b></span><br>When attending your appointment please wait in the Reception waiting room and ring 2050 then choose option 1 (Collect mobile phone).
             (01392 382050 from a mobile phone)</p>' WHERE Name = 'Collection L87';
             """);
-            
-        return sb.ToString();
+
+            return sb.ToString();
+        }
     }
 }
 

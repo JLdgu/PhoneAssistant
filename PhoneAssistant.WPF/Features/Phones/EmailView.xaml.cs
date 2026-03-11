0717f95c9c33d3475e71ddb62501695e5e9e557f
JLdgu <jonathan.linstead@devon.gov.uk>
2026-03-11 08:32:08 +0000

    Browser background not being set to grey in production
    Only happens when Windows Dark  mode is selected

--- PhoneAssistant.WPF/Features/Phones/EmailView.xaml.cs
+++ PhoneAssistant.WPF/Features/Phones/EmailView.xaml.cs
@@ -1,20 +1,35 @@
-﻿using System.Windows.Controls;
+﻿using System.ComponentModel;
+using System.Windows.Controls;
 
 namespace PhoneAssistant.WPF.Features.Phones;
 
 public partial class EmailView : UserControl
-{    
+{
     public EmailView()
     {
         InitializeComponent();
-        InitializeAsync();
-    }
-    async void InitializeAsync()
-    {
 
-        await WebView2.EnsureCoreWebView2Async(null);        
-        //WebView2.CoreWebView2.Profile.PreferredColorScheme = Microsoft.Web.WebView2.Core.CoreWebView2PreferredColorScheme.Light;
-        WebView2.DefaultBackgroundColor = System.Drawing.Color.LightGray;
+        // Don't run the WebView2 initialization when the XAML designer is active
+        if (DesignerProperties.GetIsInDesignMode(this))
+            return;
+
+        _ = InitializeAsync();
+    }
+    async Task InitializeAsync()
+    {
+        try
+        {
+            if (WebView2 is null)
+                return;
+
+            await WebView2.EnsureCoreWebView2Async(null);
+            WebView2.CoreWebView2?.Profile.PreferredColorScheme = Microsoft.Web.WebView2.Core.CoreWebView2PreferredColorScheme.Light;
+            WebView2.DefaultBackgroundColor = System.Drawing.Color.LightGray;
+        }
+        catch (Exception)
+        {
+            // Swallow exceptions during initialization to avoid crashing the XAML designer
+        }
     }
     private void DeliveryAddress_TextChanged(object sender, TextChangedEventArgs e)
     {
